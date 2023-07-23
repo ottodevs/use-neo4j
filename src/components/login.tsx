6dbbb3951175f48c59ce81be3c9508abf9ac69eb
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-23 05:45:41 +0200

    try fixing nextjs compat

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,64 +1,80 @@
-import React, { useState } from 'react'
-import { Neo4jScheme, Neo4jConfig } from '../neo4j-config.interface'
-import { globalClassNames } from '../neo4j.constants'
-import { LoginDesktop } from './login-desktop'
-import { LoginForm } from './login-form'
-
+import React, { useState } from "react";
+import { Neo4jScheme, Neo4jConfig } from "../neo4j-config.interface";
+import { globalClassNames } from "../neo4j.constants";
+import { LoginDesktop } from "./login-desktop";
+import { LoginForm } from "./login-form";
 
 interface LoginProps {
-    error?: Error;
-    onSubmit: (config: Neo4jConfig) => void;
-    scheme?: Neo4jScheme;
-    host?: string;
-    port?: string | number;
-    username?: string;
-    password?: string;
-    database?: string;
+  error?: Error;
+  onSubmit: (config: Neo4jConfig) => void;
+  scheme?: Neo4jScheme;
+  host?: string;
+  port?: string | number;
+  username?: string;
+  password?: string;
+  database?: string;
 
-    showHost?: boolean;
-    showActive?: boolean;
-    showProject?: boolean;
-    showDatabase?: boolean;
+  showHost?: boolean;
+  showActive?: boolean;
+  showProject?: boolean;
+  showDatabase?: boolean;
 
-    title?: React.ReactNode | React.ReactNode[] | null;
-    logo?: React.ReactNode | React.ReactNode[] | null;
-    details?: React.ReactNode | React.ReactNode[] | null;
-    footer?: React.ReactNode | React.ReactNode[] | null;
+  title?: React.ReactNode | React.ReactNode[] | null;
+  logo?: React.ReactNode | React.ReactNode[] | null;
+  details?: React.ReactNode | React.ReactNode[] | null;
+  footer?: React.ReactNode | React.ReactNode[] | null;
 }
 
 export const Login: React.FC<LoginProps> = ({ error, onSubmit, ...props }) => {
-    // @ts-ignore
-    const [showProjectForm, setShowProjectForm] = useState((props.showProject || props.showProject === undefined) && window.neo4jDesktopApi)
+  // @ts-ignore
+  const [showProjectForm, setShowProjectForm] = useState(
+    (props.showProject || props.showProject === undefined) &&
+      window !== undefined &&
+      (window as any)?.neo4jDesktopApi
+  );
 
-    // TODO: Dynamic classes
-    const classNames = globalClassNames.semantic
+  // TODO: Dynamic classes
+  const classNames = globalClassNames.semantic;
 
-    let form
+  let form: any;
 
-    if ( showProjectForm ) {
-        form = <LoginDesktop classNames={classNames} showActive={props.showActive} hide={() => setShowProjectForm(false)} onSubmit={onSubmit} />
-    }
-    else {
-        // @ts-ignore
-        form = <LoginForm classNames={classNames} showHost={props.showHost} showDatabase={props.showDatabase} onSubmit={onSubmit} {...props} />
-    }
+  if (showProjectForm) {
+    form = (
+      <LoginDesktop
+        classNames={classNames}
+        showActive={props.showActive}
+        hide={() => setShowProjectForm(false)}
+        onSubmit={onSubmit}
+      />
+    );
+  } else {
+    form = (
+      // @ts-ignore
+      <LoginForm
+        classNames={classNames}
+        showHost={props.showHost as any}
+        showDatabase={props.showDatabase as any}
+        onSubmit={onSubmit}
+        {...props}
+      />
+    );
+  }
 
-    return (
-        <div className={classNames.login}>
-            <div className={classNames.loginContainer}>
-                {props.title}
-                <form className={classNames.form}>
-                    {props.logo}
-                    {error && (<div className={classNames.formError}>{error.message}</div>)}
+  return (
+    <div className={classNames.login}>
+      <div className={classNames.loginContainer}>
+        {props.title}
+        <form className={classNames.form}>
+          {props.logo}
+          {error && <div className={classNames.formError}>{error.message}</div>}
 
-                    {form}
+          {form}
 
-                    {props.details}
-                </form>
+          {props.details}
+        </form>
 
-                {props.footer}
-            </div>
-        </div>
-
-    )
-}
+        {props.footer}
+      </div>
+    </div>
+  );
+};
--- src/neo4j.provider.tsx
+++ src/neo4j.provider.tsx
@@ -1,150 +1,190 @@
-import React, { useEffect, useState } from 'react'
-import { auth, Driver, AuthToken } from 'neo4j-driver'
-import { Login } from './components/login'
-import neo4j from 'neo4j-driver'
+import React, { useEffect, useState } from "react";
+import { auth, Driver, AuthToken } from "neo4j-driver";
+import { Login } from "./components/login";
+import neo4j from "neo4j-driver";
 
-import { Neo4jContext } from './neo4j.context'
-import { createDriver } from './driver'
-import { LOCAL_STORAGE_KEY, Neo4jConfig, Neo4jScheme } from './neo4j-config.interface'
+import { Neo4jContext } from "./neo4j.context";
+import { createDriver } from "./driver";
+import {
+  LOCAL_STORAGE_KEY,
+  Neo4jConfig,
+  Neo4jScheme,
+} from "./neo4j-config.interface";
 
 interface Neo4jProviderProps {
-    children: React.ReactNode | React.ReactNode[] | null;
-    driver?: Driver;
-    scheme?: Neo4jScheme;
-    host?: string;
-    port?: string | number;
-    username?: string;
-    password?: string;
-    database?: string;
+  children: React.ReactNode | React.ReactNode[] | null;
+  driver?: Driver;
+  scheme?: Neo4jScheme;
+  host?: string;
+  port?: string | number;
+  username?: string;
+  password?: string;
+  database?: string;
 
-    showHost?: boolean;
-    showDatabase?: boolean;
-    showActive?: boolean;
+  showHost?: boolean;
+  showDatabase?: boolean;
+  showActive?: boolean;
 
-    title?: React.ReactNode | React.ReactNode[] | null;
-    logo?: React.ReactNode | React.ReactNode[] | null;
-    details?: React.ReactNode | React.ReactNode[] | null;
-    footer?: React.ReactNode | React.ReactNode[] | null;
+  title?: React.ReactNode | React.ReactNode[] | null;
+  logo?: React.ReactNode | React.ReactNode[] | null;
+  details?: React.ReactNode | React.ReactNode[] | null;
+  footer?: React.ReactNode | React.ReactNode[] | null;
 }
 
-export const Neo4jProvider: React.FC<Neo4jProviderProps> = (props: Neo4jProviderProps) => {
-    const configFromStorage: Neo4jConfig = (window.localStorage ? JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || '{}') : {}) as Neo4jConfig
+export const getWindow = () => {
+  if (typeof window !== "undefined") {
+    // If window is defined (client-side), return it
+    return window;
+  } else {
+    // If window is not defined (server-side), return a dummy object
+    return {};
+  }
+};
 
-    const [ authenticating, setAuthenticating ] = useState<boolean>(true)
-    const [ config, setConfig ] = useState<Neo4jConfig>(configFromStorage)
-    const [ driver, setDriver ] = useState<Driver | undefined>(props.driver)
+export const Neo4jProvider: React.FC<Neo4jProviderProps> = (
+  props: Neo4jProviderProps
+) => {
+  const window: any = getWindow();
+  const configFromStorage: Neo4jConfig = (
+    window?.localStorage
+      ? JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || "{}")
+      : {}
+  ) as Neo4jConfig;
 
-    const [ error, setError ] = useState<Error>()
-    const [ database, setDatabase ] = useState<string | undefined>(props.database)
+  const [authenticating, setAuthenticating] = useState<boolean>(true);
+  const [config, setConfig] = useState<Neo4jConfig>(configFromStorage);
+  const [driver, setDriver] = useState<Driver | undefined>(props.driver);
 
+  const [error, setError] = useState<Error>();
+  const [database, setDatabase] = useState<string | undefined>(props.database);
 
-    const updateConnection = (config: Neo4jConfig) => {
-        setConfig(config)
-        setDatabase(database)
-        setAuthenticating(true)
+  const updateConnection = (config: Neo4jConfig) => {
+    setConfig(config);
+    setDatabase(database);
+    setAuthenticating(true);
 
-        const newDriver = createDriver(config.scheme, config.host, config.port, config.username, config.password)
+    const newDriver = createDriver(
+      config.scheme,
+      config.host,
+      config.port,
+      config.username,
+      config.password
+    );
 
-        newDriver.verifyConnectivity()
-            .then(() => {
-                setDriver(newDriver)
+    newDriver
+      .verifyConnectivity()
+      .then(() => {
+        setDriver(newDriver);
+        window?.localStorage &&
+          window.localStorage.setItem(
+            LOCAL_STORAGE_KEY,
+            JSON.stringify(config)
+          );
+      })
+      .catch((e) => setError(e))
+      .finally(() => setAuthenticating(false));
+  };
 
-                window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(config))
-            })
-            .catch(e => setError(e))
-            .finally(() => setAuthenticating(false))
+  useEffect(() => {
+    // Has Driver been Passed?
+    if (props.driver) {
+      props.driver
+        .verifyConnectivity()
+        .catch((e) => setError(e))
+        .finally(() => {
+          setDriver(props.driver);
+          setAuthenticating(false);
+        });
+    }
+    // Has a connection string been provided to the url?
+    else {
+      setAuthenticating(false);
+    }
+  }, [driver]);
+
+  // Test driver passed as a prop or url search params
+  useEffect(() => {
+    if (props.driver !== undefined) {
+      return;
     }
 
-    useEffect(() => {
-        // Has Driver been Passed?
-        if ( props.driver ) {
-            props.driver.verifyConnectivity()
-                .catch(e => setError(e))
-                .finally(() => {
-                    setDriver(props.driver)
-                    setAuthenticating(false)
-                })
+    if (window?.location?.search !== undefined) {
+      const searchParams = new URLSearchParams(window.location.search);
+
+      // Attempt to connect from URL parameters
+      let urlScheme: any, urlHost: any, urlPort: any;
+
+      if (searchParams.has("url")) {
+        const url = searchParams.get("url")!;
+
+        const matches = url.match(
+          /^((neo4j|bolt)((\+s?(sc)?)?):\/\/)?([\w.]+)(:([0-9]+))?$/
+        );
+
+        if (matches) {
+          urlScheme = matches[2] || "neo4j";
+          urlHost = matches[6];
+          urlPort = matches[8] || 7687;
         }
-        // Has a connection string been provided to the url?
-        else {
-            setAuthenticating(false)
+      }
+
+      if (urlScheme && urlHost && urlPort) {
+        // Build Credentials
+        const username = searchParams.get("user") || undefined;
+        const password = searchParams.get("pass") || undefined;
+
+        // Attempt to Connect
+        const config: Neo4jConfig = {
+          scheme: urlScheme || "neo4j",
+          host: urlHost || "localhost",
+          port: urlPort || 7687,
+          username,
+          password,
+          database: searchParams.get("database") || undefined,
+        };
+
+        if (config) {
+          updateConnection(config);
         }
-    }, [driver])
-
-    // Test driver passed as a prop or url search params
-    useEffect(() => {
-        if ( props.driver !== undefined)  {
-            return
-        }
-
-        const searchParams = new URLSearchParams(window.location.search)
-
-        // Attempt to connect from URL parameters
-        let urlScheme, urlHost, urlPort
-
-        if ( searchParams.has('url') ) {
-            const url = searchParams.get('url')!
-
-            const matches = url.match(/^((neo4j|bolt)((\+s?(sc)?)?):\/\/)?([\w.]+)(:([0-9]+))?$/)
-
-            if (matches) {
-                urlScheme = matches[2] || 'neo4j'
-                urlHost = matches[6]
-                urlPort = matches[8]  || 7687
-            }
-        }
-
-        if (urlScheme && urlHost && urlPort) {
-            // Build Credentials
-            const username = searchParams.get('user') || undefined
-            const password = searchParams.get('pass') || undefined
-
-            // Attempt to Connect
-            const config: Neo4jConfig = {
-                scheme: urlScheme || 'neo4j',
-                host: urlHost || 'localhost',
-                port: urlPort || 7687,
-                username,
-                password,
-                database: searchParams.get('database') || undefined
-            }
-
-            updateConnection(config)
-        }
-    }, [ props.driver ])
-
-
-    // Wait for effect to verify driver connectivity
-    if ( authenticating ) {
-        return(<div className="authenticating"></div>)
+      }
     }
+  }, [props.driver]);
 
-    if ( !driver ) {
-        const { title, logo, details, footer } = props;
+  // Wait for effect to verify driver connectivity
+  if (authenticating) {
+    return <div className="authenticating"></div>;
+  }
 
-        return (<Login
-            error={error}
-            title={title}
-            logo={logo}
-            details={details}
-            footer={footer}
-            scheme={config.scheme}
-            host={config.host}
-            port={config.port}
-            username={config.username}
-            password={config.password}
-            database={config.database}
-            showActive={props.showActive}
-            showHost={props.showHost}
-            showDatabase={props.showDatabase}
-            onSubmit={updateConnection}
-            {...props}
-        />)
-    }
+  if (!driver) {
+    const { title, logo, details, footer } = props;
 
     return (
-        <Neo4jContext.Provider value={{ driver, config, database, updateConnection, setDatabase, }}>
-            {props.children}
-        </Neo4jContext.Provider>
-    )
-}
+      <Login
+        error={error}
+        title={title}
+        logo={logo}
+        details={details}
+        footer={footer}
+        scheme={config.scheme}
+        host={config.host}
+        port={config.port}
+        username={config.username}
+        password={config.password}
+        database={config.database}
+        showActive={props.showActive}
+        showHost={props.showHost}
+        showDatabase={props.showDatabase}
+        onSubmit={updateConnection}
+        {...props}
+      />
+    );
+  }
+
+  return (
+    <Neo4jContext.Provider
+      value={{ driver, config, database, updateConnection, setDatabase }}
+    >
+      {props.children}
+    </Neo4jContext.Provider>
+  );
+};
